2c72ab0c9fea9cf26a45a6f6e58e27a75095112b
ElGnarly <misavke@gmail.com>
2019-03-09 14:46:24 +0300

    changed user dependencies

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:success] = "Article was successfully created"
 			redirect_to article_path(@article)
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
 	def update
 		
 		if @article.update(article_params)
-			flash[:success] = "article was successfully updated"
+			flash[:success] = "Article was successfully updated"
 			redirect_to article_path(@article)
 		else
 			render 'edit'
